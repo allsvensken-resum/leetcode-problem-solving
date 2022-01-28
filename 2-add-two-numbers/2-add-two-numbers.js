dc9e9f33d840d37d089aa2d65e9b800f7a97e96e
suppakorn-dev <95694340+suppakorn-dev@users.noreply.github.com>
2022-01-28 15:45:14 +0700

    Time: 236 ms (13.62%), Space: 50.7 MB (5.39%) - LeetHub

---
+++ 2-add-two-numbers/2-add-two-numbers.js
@@ -0,0 +1,63 @@
+/**
+ * Definition for singly-linked list.
+ * function ListNode(val, next) {
+ *     this.val = (val===undefined ? 0 : val)
+ *     this.next = (next===undefined ? null : next)
+ * }
+ */
+/**
+ * @param {ListNode} l1
+ * @param {ListNode} l2
+ * @return {ListNode}
+ */
+const addTwoNumbers = function(l1, l2) {
+    let aCurrent = l1;
+    let bCurrent = l2;
+    let previousRemain = 0;
+    let currNewList = null;
+    let headNode = null;
+    
+    while(aCurrent || bCurrent) {
+        let aCurrentVal = 0;
+        let bCurrentVal = 0;
+        
+        if (aCurrent) {
+            aCurrentVal = aCurrent.val;
+            aCurrent = aCurrent.next;
+        }
+        
+        if (bCurrent) {
+            bCurrentVal = bCurrent.val;
+            bCurrent = bCurrent.next;
+        }
+        
+        let sum = aCurrentVal + bCurrentVal + previousRemain;
+        previousRemain = 0;
+        
+        if (sum > 9) {
+            let [remaining, newSum] = splitToDigit(sum.toString());     
+            sum = newSum;
+            previousRemain = remaining;
+        }
+        
+        if (!currNewList) {
+            currNewList = new ListNode(sum);
+            headNode = currNewList;
+            continue;
+        }
+        
+        currNewList.next = new ListNode(sum);
+        currNewList = currNewList.next;
+    }
+    
+    if (previousRemain !== 0) {
+        currNewList.next = new ListNode(previousRemain);
+    }
+    
+    console.log(headNode);
+    return headNode;
+};
+
+const splitToDigit = (numberString) => {
+    return numberString.split("").map(Number);
+}
