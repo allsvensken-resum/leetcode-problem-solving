6e6a402d3f54b04f90fc62e9c7854c7482a55eef
suppakorn-dev <95694340+suppakorn-dev@users.noreply.github.com>
2022-02-07 16:59:11 +0700

    Time: 105 ms (63.87%), Space: 51.7 MB (26.37%) - LeetHub

---
+++ 121-best-time-to-buy-and-sell-stock/121-best-time-to-buy-and-sell-stock.js
@@ -0,0 +1,27 @@
+/**
+ * @param {number[]} prices
+ * @return {number}
+ */
+
+/**
+    - The problem is finding the most diff value of array.
+*/
+const maxProfit = function(prices) {
+    let minPrice = Infinity;
+    let maxProfit = 0;
+    for (let i = 0; i < prices.length; i++) {
+        if (prices[i] < minPrice) {
+            minPrice = prices[i];
+        } else if (prices[i] - minPrice > maxProfit) {
+            maxProfit = prices[i] - minPrice;
+        } 
+    }
+    
+    return maxProfit;
+};
+
+
+
+
+
+
